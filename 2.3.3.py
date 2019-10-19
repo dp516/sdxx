bd6b429253503b734d6c249bd4827ed907726a54
stephendp <roben9527@163.com>
2019-10-20 01:24:14 +0800

    first

---
+++ 2.3.3.py
@@ -0,0 +1,60 @@
+'''
+@Descripttion: 
+@version: 
+@Author: stephendp
+@Date: 2019-10-20 01:13:35
+@LastEditors: stephendp
+@LastEditTime: 2019-10-20 01:22:47
+'''
+import numpy as np
+
+"""
+与门
+"""
+def AND(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5]) # 权重
+    b = -0.7 # 偏置。决定了神经元被激活的容易程度
+    
+    tmp = np.sum(w*x) + b
+
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+"""
+非门
+"""
+def NAND(x1, x2):
+    x = np.array([x1, x2])
+    # 权重和偏置不同
+    w = np.array([-0.5, -0.5]) # 权重
+    b = 0.7 # 偏置。决定了神经元被激活的容易程度
+    
+    tmp = np.sum(w*x) + b
+
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+"""
+或门
+"""
+def OR(x1, x2):
+    x = np.array([x1, x2])
+    # 权重和偏置不同
+    w = np.array([0.5, 0.5]) # 权重
+    b = -0.2 # 偏置。决定了神经元被激活的容易程度
+    
+    tmp = np.sum(w*x) + b
+
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+print(AND(1, 1))
+print(NAND(1, 1))
+print(OR(1, 1))
